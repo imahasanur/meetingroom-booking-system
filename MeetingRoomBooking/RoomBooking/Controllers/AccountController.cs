4373c742bc6252d3be8bd801d29745d9790416f3
Ahasanur Rahman Mehul <ahasan.onnorokom@gmail.com>
2024-12-10 15:21:13 +0600

    added registration page with get request action

--- MeetingRoomBooking/RoomBooking/Controllers/AccountController.cs
+++ MeetingRoomBooking/RoomBooking/Controllers/AccountController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RoomBooking.Models;
 
 namespace RoomBooking.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Register()
+        {
+            var model = new RegistrationModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegistrationModel model)
         {
             return View();
         }
---
+++ MeetingRoomBooking/RoomBooking/Models/RegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace RoomBooking.Models
+{
+    public class RegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+}
