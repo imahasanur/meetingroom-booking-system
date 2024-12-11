a3882830c193a2206147f60a1559f0efd66c4246
Ahasanur Rahman Mehul <ahasan.onnorokom@gmail.com>
2024-12-11 09:05:40 +0600

    added iunitofWork and Repositories folder to Domain project

---
+++ MeetingRoomBooking/RoomBooking.Domain/Entities/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBooking.Domain.Entities
+{
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime? LastUpdatedAtUtc { get; set; }
+    }
+}
