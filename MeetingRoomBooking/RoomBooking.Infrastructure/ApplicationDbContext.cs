2a1186e147d3a695c3a0ee77aebd26f4b54f53e1
Ahasanur Rahman Mehul <ahasan.onnorokom@gmail.com>
2024-12-11 17:44:48 +0600

    seed an admin with a claim and value

--- MeetingRoomBooking/RoomBooking.Infrastructure/ApplicationDbContext.cs
+++ MeetingRoomBooking/RoomBooking.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RoomBooking.Infrastructure.Membership;
 using System;
@@ -34,6 +35,54 @@ namespace RoomBooking.Infrastructure
             base.OnConfiguring(optionsBuilder);
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
+            var adminPassword = Environment.GetEnvironmentVariable("AdminPassword");
+            var adminIdentity = Environment.GetEnvironmentVariable("AdminIdentification");
+            var adminIdentityUpper = Environment.GetEnvironmentVariable("AdminIdentificationUpper");
+            var adminFirstName = Environment.GetEnvironmentVariable("AdminFirstName");
+            var adminLastName = Environment.GetEnvironmentVariable("AdminLastName");
+            var adminFullName = Environment.GetEnvironmentVariable("AdminFullName");
+
+            var passwordHasher = new PasswordHasher<ApplicationUser>();
+
+            var adminUserId = new Guid("6C23FDA7-AE43-439E-B7F9-7D30868CB399");
+            var adminUser = new ApplicationUser
+            {
+                Id = adminUserId,
+                UserName = adminIdentity,
+                NormalizedUserName = adminIdentityUpper,
+                Email = adminIdentity,
+                NormalizedEmail = adminIdentityUpper,
+                EmailConfirmed = true,
+                SecurityStamp = string.Empty,
+                FirstName = adminFirstName,
+                LastName = adminLastName,
+                FullName = adminFullName,
+                CreatedAtUtc = new DateTime(2024, 12, 11, 0, 0, 0, DateTimeKind.Utc)
+            };
+            adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, adminPassword);
+
+            builder.Entity<ApplicationUser>().HasData(adminUser);
+
+            builder.Entity<ApplicationUserClaim>().HasData(
+                new ApplicationUserClaim
+                {
+                    Id = 1,
+                    UserId = adminUserId,
+                    ClaimType = "role",
+                    ClaimValue = "admin"
+                },
+                new ApplicationUserClaim
+                {
+                    Id = 2,
+                    UserId = adminUserId,
+                    ClaimType = "role",
+                    ClaimValue = "user"
+                }
+            );
+        }
     }
 }
---
+++ MeetingRoomBooking/RoomBooking/Data/Migrations/20241211112155_SeedAdminData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RoomBooking.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdminData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "CreatedAtUtc", "Email", "EmailConfirmed", "FirstName", "FullName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UpdatedAtUtc", "UserName" },
+                values: new object[] { new Guid("6c23fda7-ae43-439e-b7f9-7d30868cb399"), 0, "ebc76ddd-20bc-4a88-8c39-abc26e8a3f06", new DateTime(2024, 12, 11, 0, 0, 0, 0, DateTimeKind.Utc), "admin@gmail.com", true, "Admin", "Admin User", "User", false, null, "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", "AQAAAAIAAYagAAAAEI6RkZ6/eFwXkjylbOJOmWEU9IfVGISPPfStWFC4Ms1ddfdYosFj0KWjHKWjqQGljg==", null, false, "", false, null, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserClaims",
+                columns: new[] { "Id", "ClaimType", "ClaimValue", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "role", "admin", new Guid("6c23fda7-ae43-439e-b7f9-7d30868cb399") },
+                    { 2, "role", "user", new Guid("6c23fda7-ae43-439e-b7f9-7d30868cb399") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserClaims",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserClaims",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("6c23fda7-ae43-439e-b7f9-7d30868cb399"));
+        }
+    }
+}
