d66e39d619b0111ad063789fc37e0bbc0cda60f0
Ahasanur Rahman Mehul <ahasan.onnorokom@gmail.com>
2024-12-11 12:48:03 +0600

    Edited LoginModel and view

--- MeetingRoomBooking/RoomBooking/Models/LoginModel.cs
+++ MeetingRoomBooking/RoomBooking/Models/LoginModel.cs
@@ -1,9 +1,20 @@
-﻿namespace RoomBooking.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomBooking.Models
 {
     public class LoginModel
     {
-        public string Email { get; set; }   
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 7)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
-        public string ReturnUrl { get; set; }
+        public string? ReturnUrl { get; set; }
+
     }
 }
