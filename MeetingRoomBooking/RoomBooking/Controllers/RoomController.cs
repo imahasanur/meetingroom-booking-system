3d5f5b2c18873047935e679a5db38adc3c017bf6
Ahasanur Rahman Mehul <ahasan.onnorokom@gmail.com>
2024-12-11 18:09:18 +0600

    Made actions of Room Controller and necessary views

---
+++ MeetingRoomBooking/RoomBooking/Controllers/RoomController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomBooking.Controllers
+{
+    public class RoomController : Controller
+    {
+
+        public IActionResult Get()
+        { 
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
